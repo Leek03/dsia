7ad053b813a2aa188f0884af0b6d072b550cacbf
E A Thomas <earnest.thomas@gmail.com>
2019-07-11 21:56:02 +1000

    latest

---
+++ leek/Module 1/num1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 12:41:36 2019
+
+
+
+#import numpy as np
+#import pandas as pd
+import seaborn as sns
+import plotly
+%matplotlib inline
+import plotly.plotly as py
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+# Create table
+conn.execute('''CREATE TABLE COMPANY(ID INT PRIMARY KEY     NOT NULL,
+                NAME           TEXT    NOT NULL,
+                AGE            INT     NOT NULL,
+                ADDRESS        CHAR(50),SALARY         REAL);''')
+                print "Table created successfully";
+                conn.()
